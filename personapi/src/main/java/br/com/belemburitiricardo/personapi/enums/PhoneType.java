b99a86692069e60bba48e83fee0f9c56a7ad0dc6
ricardobad51 <rbbelem@gmail.com>
2021-12-21 23:35:07 -0300

    Add Entities Person e Phone

---
+++ personapi/src/main/java/br/com/belemburitiricardo/personapi/enums/PhoneType.java
@@ -0,0 +1,20 @@
+package br.com.belemburitiricardo.personapi.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+	HOME("Home"),
+	MOBILE("Mobile"),
+	COMMERCIAL("Commercial");
+	
+	PhoneType(String string) {
+		this.description = "";
+		// TODO Auto-generated constructor stub
+	}
+
+	private final String description;
+}
